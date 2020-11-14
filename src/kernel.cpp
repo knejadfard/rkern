6ab72d0db06d899cad41cfaebae8d86389cfbb9e
Kian Nejadfard <k.nejadfard@gmail.com>
2020-11-14 12:43:07 -0500

    Compile kernel code as C++
    A separate makefile named Makefile.clang has been added for building
    the kernel using the LLVM toolchain.
    The original makefile has been renamed to Makefile.gcc to keep a
    similar naming convention.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,18 +1,22 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
- 
-/* Check if the compiler thinks you are targeting the wrong operating system. */
+// Check if the compiler thinks the wrong operating system is being targetted.
 #if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#error "Not using a cross-compiler. This is probably a bad idea."
 #endif
- 
-/* This tutorial will only work for the 32-bit ix86 targets. */
+
+// Currently rkern only works for 32-bit ix86 targets.
 #if !defined(__i386__)
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
- 
-/* Hardware text mode color constants. */
+
+using size_t = unsigned int;
+using uint8_t = unsigned int;
+using uint16_t = unsigned long int;
+using uint32_t = unsigned long long int;
+using int8_t = int;
+using int16_t = long int;
+using int32_t = long long int;
+
+// Hardware text mode color constants.
 enum vga_color {
 	VGA_COLOR_BLACK = 0,
 	VGA_COLOR_BLUE = 1,
@@ -31,34 +35,34 @@ enum vga_color {
 	VGA_COLOR_LIGHT_BROWN = 14,
 	VGA_COLOR_WHITE = 15,
 };
- 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
+
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
 }
- 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
+
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
- 
-size_t strlen(const char* str) 
+
+size_t strlen(const char* str)
 {
 	size_t len = 0;
 	while (str[len])
 		len++;
 	return len;
 }
- 
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
- 
+
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
- 
-void terminal_initialize(void) 
+
+void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
@@ -71,19 +75,19 @@ void terminal_initialize(void)
 		}
 	}
 }
- 
-void terminal_setcolor(uint8_t color) 
+
+void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
- 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
- 
-void terminal_putchar(char c) 
+
+void terminal_putchar(char c)
 {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
@@ -92,23 +96,22 @@ void terminal_putchar(char c)
 			terminal_row = 0;
 	}
 }
- 
-void terminal_write(const char* data, size_t size) 
+
+void terminal_write(const char* data, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
- 
-void terminal_writestring(const char* data) 
+
+void terminal_writestring(const char* data)
 {
 	terminal_write(data, strlen(data));
 }
- 
-void kernel_main(void) 
+
+extern "C" void kernel_main()
 {
-	/* Initialize terminal interface */
+	// Initialize terminal interface
 	terminal_initialize();
- 
-	/* Newline support is left as an exercise. */
+
 	terminal_writestring("Hello, kernel World!\n");
 }
