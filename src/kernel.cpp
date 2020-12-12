a1d3ddcdc5eef81897b58c5be86f4e9532f9db08
Kian Nejadfard <k.nejadfard@gmail.com>
2020-12-11 20:25:06 -0500

    Remove i386 enforcement macro
    Since we are moving completely to RISC-V, this is wrong now.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -3,11 +3,6 @@
   #error "Not using a cross-compiler. This is probably a bad idea."
 #endif
 
-// Currently rkern only works for 32-bit ix86 targets.
-#if !defined(__i386__)
-  #error "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
-
 #include "types.hpp"
 #include "vga.hpp"
 #include "utility.hpp"
