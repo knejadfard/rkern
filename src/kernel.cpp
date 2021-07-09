51a2c1568c717b00a1bddf86f5fb623edc0ca9a4
Kian Nejadfard <k.nejadfard@gmail.com>
2021-07-09 11:15:29 -0400

    Refactor code and add remaining gpio functions per spec

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -11,19 +11,7 @@ extern "C" void kernel_main()
 
     rkern::driver::Gpio gpio0{gpio0_base};
 
-    gpio0.disable_input(19);
-    gpio0.disable_io_function(19);
-    gpio0.enable_output(19);
-
     gpio0.disable_input(21);
     gpio0.disable_io_function(21);
     gpio0.enable_output(21);
-
-    gpio0.disable_input(22);
-    gpio0.disable_io_function(22);
-    gpio0.enable_output(22);
-
-        gpio0.set_output_value(19, 1);
-        gpio0.set_output_value(21, 1);
-        gpio0.set_output_value(22, 1);
 }
